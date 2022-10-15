a4660ea4f2e349c84becf45961b77607537c08f0
ATIENOSYL <atienosylvia03@gmail.com>
2022-10-15 16:48:39 +0300

    javascriptfundamentals

---
+++ literals.js
@@ -0,0 +1,6 @@
+console.log('I counted ${10 + 23} goats');
+
+let firstName = 'Sylvia';
+let lastName = 'Makokha';
+let fullName =firstName '' + lastName;
+console.log(fullName);
---
+++ strings.js
@@ -0,0 +1,13 @@
+let annoyingLaugh= 'teehee so funny! teehee!';
+annoyingLaugh.replace('teehee', 'haha');
+console.log(annoyingLaugh);
+"pecan pie"
+const age = "5" + "4";
+console.log(age);
+"pecan pie"(7);
+console.log("pecan pie");
+console.log(annoyingLaugh.replaceAll('teehee', 'haha'));
+
+// new line
+console.log('hello\nworld');
+console.log('hello\'world\');
